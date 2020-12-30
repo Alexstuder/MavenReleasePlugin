1401b4934a25f1442e0adc7d61c5f4472801f566
Alex Studer <alex@alexstuder.ch>
2020-12-30 22:30:47 +0100

    pom for maven-release-plugin added

--- DemoMavenReleasePlugin/src/main/java/ch/studer/DemoMavenReleasePlugin.java
+++ DemoMavenReleasePlugin/src/main/java/ch/studer/DemoMavenReleasePlugin.java
@@ -1,10 +1,14 @@
+package ch.studer;
+
+
+
 public class DemoMavenReleasePlugin {
     public static void main(String args[]){
 
         System.out.println("Hello Explorer");
         System.out.println();
         System.out.println();
-        System.out.println("This progrm is running version : "+
+        System.out.println("This program is running version : "+
                 DemoMavenReleasePlugin.class.getPackage().getImplementationVersion());
 
     }
