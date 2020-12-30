79f6590baf67c7ac524dcb40678da42602ce2bfe
Alex Studer <alex@alexstuder.ch>
2020-12-30 23:02:51 +0100

    release.properties added

--- DemoMavenReleasePlugin/src/main/java/ch/studer/DemoMavenReleasePlugin.java
+++ DemoMavenReleasePlugin/src/main/java/ch/studer/DemoMavenReleasePlugin.java
@@ -11,5 +11,9 @@ public class DemoMavenReleasePlugin {
         System.out.println("This program is running version : "+
                 DemoMavenReleasePlugin.class.getPackage().getImplementationVersion());
 
+        System.out.println("mvn package   ==> build  ==> in \\target");
+        System.out.println("mvn release:prepare   ==> release and push to github");
+        System.out.println("mvn release:perform   ==> get the release from github an put it to \\target\\checkout\\AppName\\target\\");
+
     }
 }
