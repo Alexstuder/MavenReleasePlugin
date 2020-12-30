840c8791c8b46448c05d00efa3d4ebf1dba11954
Alex Studer <alex@alexstuder.ch>
2020-12-30 18:38:39 +0100

    main added

--- DemoMavenReleasePlugin/src/main/java/DemoMavenReleasePlugin.java
+++ DemoMavenReleasePlugin/src/main/java/DemoMavenReleasePlugin.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class DemoMavenReleasePlugin {
+public class DemoMavenReleasePlugin {
+    public static void main(String args[]){
+
+        System.out.println("Hello Explorer");
+        System.out.println();
+        System.out.println("This progrm is running version : "+
+                DemoMavenReleasePlugin.class.getPackage().getImplementationVersion());
+
+    }
 }
